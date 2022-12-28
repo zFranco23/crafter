89e3ce0e32458b97ae24dc769eb3599a2eb738fc
zFranco23 <francoheflo@gmail.com>
2022-12-27 23:32:18 -0500

    Updated title

--- src/components/Ground/Ground.tsx
+++ src/components/Ground/Ground.tsx
@@ -2,13 +2,15 @@ import { FC } from "react";
 import { usePlane } from "@react-three/cannon";
 import { groundTexture } from "../../constants/textures";
 
+groundTexture.repeat.set(100,100)
+
 const Ground: FC = (props: any) => {
   const [ref] = usePlane(() => ({
     rotation: [- Math.PI/ 2,0,0], //plane in xyz
     position: [0,-0.5,0]
   })) 
 
-  groundTexture.repeat.set(100,100)
+  
   return (
     <mesh ref={ref} {...props}>
       {/* attach to mesh geometry */}
